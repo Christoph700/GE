76d3d20c4b8a5007ac3e4cfa220dfdb52fc4b459
Christoph700 <36846939+Christoph700@users.noreply.github.com>
2018-03-26 15:47:54 +0200

    first set renderer color then fill rect

--- GE/Ball.cpp
+++ GE/Ball.cpp
@@ -20,8 +20,8 @@ void Ball::render(SDL_Renderer* renderer)
 	fillRect.y = 10;
 	fillRect.w = 100;
 	fillRect.h = 100;
-	SDL_RenderFillRect(renderer, &fillRect);
 	SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
+	SDL_RenderFillRect(renderer, &fillRect);
 }
 
 

d0ff40b0b79cf36ae4a7a19753c25f7c98f8234d
Christoph <christophsolleder@gmx.de>
2018-03-30 22:43:24 +0200

    gamelogic added

---
+++ GE/GameLogic.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "GameLogic.h"
+
+#include "Point.h"
+#include "Ball.h"
+
+GameLogic::GameLogic(SDL_Renderer* renderer) : renderer(renderer)
+{
+}
+
+
+GameLogic::~GameLogic()
+{
+}
+
+
+void GameLogic::handleMouseEvent(Point mousePosition)
+{
+	GraphicsObject *ball = new Ball(mousePosition);
+	graphicObjects.push_back(ball);
+	ball->render(renderer);
+}
---
+++ GE/GameLogic.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <vector>
+
+class GraphicsObject;
+class Point;
+struct SDL_Renderer;
+
+
+class GameLogic
+{
+public:
+	GameLogic(SDL_Renderer* renderer);
+	~GameLogic();
+	void handleMouseEvent(Point mousePosition);
+
+private:
+	std::vector<GraphicsObject*> graphicObjects;
+	SDL_Renderer* renderer;
+};
+
